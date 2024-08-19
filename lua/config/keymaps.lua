d9cb2e16f8de903205bea698cb560b24e38bbd6c
Stefano Chiodino <nope@nope.nope>
2024-08-19 01:58:39 -0700

    Initial commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,8 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- local builtin = require("telescope.builtin")
+-- vim.keymap.set("n", "<leader>sN", function()
+--   builtin.find_files({ cwd = vim.fn.stdpath("config") })
+-- end, { desc = "[s]earch [N]eovim files" })
